76ff186fe24c4befbfa6247ccc276473fe2ae673
minjun2131 <whitehodol@naver.com>
2024-12-20 15:45:43 +0900

    fix: proxy 주석

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://openapi.naver.com/v1/search/",
-        changeOrigin: true,
-        secure: false, // HTTPS 요청을 HTTP로 바꿀 때 사용
-        rewrite: (path) => path.replace(/^\/api/, ""), // "/api"를 제거하여 프록시
-      },
-    },
-  },
-});
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "https://openapi.naver.com/v1/search/",
+//         changeOrigin: true,
+//         secure: false, // HTTPS 요청을 HTTP로 바꿀 때 사용
+//         rewrite: (path) => path.replace(/^\/api/, ""), // "/api"를 제거하여 프록시
+//       },
+//     },
+//   },
+// });
